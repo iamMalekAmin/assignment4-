4bda86562afb0ed98ffd27fad2c5f0f41919b52e
Malek Amin <87621774+iamMalekAmin@users.noreply.github.com>
2022-10-31 20:51:21 +0200

    Add files via upload

---
+++ problem4.cpp
@@ -0,0 +1,46 @@
+// problem4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int power(int x, int y)
+{
+	if (y == 1)
+		return x;
+	else
+		return (x * power(x, y - 1)); //(power(x, y / 2) * power(x, y / 2));//
+}
+int power2(int x, int y)
+{
+	if (y % 2 == 0)
+	{
+		if (y == 1)
+			return x;
+		else
+			return  (power(x, y / 2) * power(x, y / 2) );
+	}
+	else
+		if (y == 1)
+			return x;
+		else
+			return  (power(x, y / 2) * power(x, y / 2) * x);
+}
+
+int main()
+{
+	cout << fixed;
+	cout << power2(2, 6) << endl; 
+	cout << power2(2 , 5)<< endl;
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
